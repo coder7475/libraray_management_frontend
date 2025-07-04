ed811f5621260b8900a3014e97a3094bb0609f09
coder7475 <robiulhossain7475@gmail.com>
2025-07-04 20:05:22 +0600

    refactor: improve code formatting and structure in bookApi service for better readability and maintainability

--- src/services/books.ts
+++ src/services/books.ts
@@ -1,34 +1,50 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import type { IBook, IBookApiReseponse, IBookResponse, IBorrowResponse, IBorrows, ICreateBookApiResponse, IDeleteBook } from './types'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import type {
+  IBook,
+  IBookApiReseponse,
+  IBookResponse,
+  IBorrowResponse,
+  IBorrows,
+  ICreateBookApiResponse,
+  IDeleteBook,
+} from "./types";
 
 export const bookApi = createApi({
-  reducerPath: 'bookApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://library-management-apis.vercel.app/api' }),
-  tagTypes: ['Books', 'Borrows'],
+  reducerPath: "bookApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://library-management-apis.vercel.app/api",
+  }),
+  tagTypes: ["Books", "Borrows"],
   endpoints: (builder) => ({
-    getBooks: builder.query<IBookApiReseponse, {page: number, limit?: number}>({
-        query: ({ page, limit = 8 }) => `books?page=${page}&limit=${limit}`,
-        providesTags: ['Books']
+    getBooks: builder.query<
+      IBookApiReseponse,
+      { page: number; limit?: number }
+    >({
+      query: ({ page, limit = 8 }) => `books?page=${page}&limit=${limit}`,
+      providesTags: ["Books"],
     }),
     getBookById: builder.query<IBookResponse, { _id: string }>({
-      query: ({ _id } ) => `books/${_id}`,
-      providesTags: ['Books']
+      query: ({ _id }) => `books/${_id}`,
+      providesTags: ["Books"],
     }),
     createBook: builder.mutation<ICreateBookApiResponse, IBook>({
       query: (newBook) => ({
-        url: '/books',
+        url: "/books",
         method: "POST",
         body: newBook,
       }),
-      invalidatesTags: ['Books'],
+      invalidatesTags: ["Books"],
     }),
-    updateBook: builder.mutation<IBookResponse, { id: string; data: Partial<IBook> }>({
+    updateBook: builder.mutation<
+      IBookResponse,
+      { id: string; data: Partial<IBook> }
+    >({
       query: ({ id, data }) => ({
         url: `/books/${id}`,
         method: "PUT",
         body: data,
       }),
-      invalidatesTags: ["Books"], 
+      invalidatesTags: ["Books"],
     }),
     deleteBook: builder.mutation<IDeleteBook, { id: string | undefined }>({
       query: ({ id }) => ({
@@ -37,27 +53,30 @@ export const bookApi = createApi({
       }),
       invalidatesTags: ["Books"],
     }),
-    borrowBook: builder.mutation<IBorrowResponse, { book: string | undefined, quantity: number, dueDate: string }>({
-      query: ({ book, quantity, dueDate}) => ({
+    borrowBook: builder.mutation<
+      IBorrowResponse,
+      { book: string | undefined; quantity: number; dueDate: string }
+    >({
+      query: ({ book, quantity, dueDate }) => ({
         url: `borrow`,
         method: "POST",
-        body: { book, quantity, dueDate }
+        body: { book, quantity, dueDate },
       }),
-      invalidatesTags: ['Books', 'Borrows']
+      invalidatesTags: ["Books"],
     }),
     getBorrowSummury: builder.query<IBorrows, void>({
       query: () => `borrow`,
-      providesTags: ['Borrows']
+      providesTags: ["Books"],
     }),
-  })  
-})
+  }),
+});
 
-export const { 
-  useGetBooksQuery, 
+export const {
+  useGetBooksQuery,
   useGetBookByIdQuery,
-  useCreateBookMutation, 
-  useUpdateBookMutation, 
-  useDeleteBookMutation, 
+  useCreateBookMutation,
+  useUpdateBookMutation,
+  useDeleteBookMutation,
   useBorrowBookMutation,
-  useGetBorrowSummuryQuery
+  useGetBorrowSummuryQuery,
 } = bookApi;
